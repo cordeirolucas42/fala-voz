38aa069a471071225596500989b26e354cb4f990
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-07-29 23:12:56 -0300

    test

--- src/components/body.js
+++ src/components/body.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import MarciaPerfil from "../images/marcia-perfil.jpg"
+import MarciaPerfil from "/src/images/marcia-perfil.jpg"
 import Programacao from "../components/programacao"
 
 const Body = () => {
